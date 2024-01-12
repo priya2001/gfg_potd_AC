20ab2cdd01e04d5e0ea5b659883124292be4794e
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-01-12 23:59:19 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Anagram of String/anagram-of-string.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+const int CHARS = 26;
+
+// function to calculate minimum numbers of characters
+// to be removed to make two strings anagram
+int remAnagram(string str1, string str2);
+
+// Driver program to run the case
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string str1,str2;
+        cin>>str1>>str2;
+        cout << remAnagram(str1, str2);
+        cout<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+// function to calculate minimum numbers of characters
+// to be removed to make two strings anagram
+int remAnagram(string str1, string str2)
+{
+// Your code goes here
+map<char,int>mp;
+for(auto it:str1)
+{
+    mp[it]++;
+}
+for(auto it:str2)
+{
+    mp[it]--;
+}
+int ans=0;
+for(auto it:mp)
+{
+    ans=ans+abs(it.second);
+}
+return ans;
+
+}
