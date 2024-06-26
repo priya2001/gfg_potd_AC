ab801cfc65f4878d98a33641aecc644367f82a6c
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-06-26 13:23:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Help Classmates/help-classmates.cpp
+++ Difficulty: Medium/Help Classmates/help-classmates.cpp
@@ -13,19 +13,52 @@ class Solution{
     public:
     vector<int> help_classmate(vector<int> arr, int n) 
     { 
-        vector<int>res(n,-1);
-        for(int i=0;i<n;i++)
+        // vector<int>res(n,-1);
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=i+1;j<n;j++)
+        //     {
+        //         if(arr[i]>arr[j])
+        //         {
+        //             res[i]=arr[j];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return res;
+        
+        stack<int>s;
+        vector<int>v;
+        for(int i=n-1;i>=0;i--)
         {
-            for(int j=i+1;j<n;j++)
+            if(s.empty())
             {
-                if(arr[i]>arr[j])
+                v.push_back(-1);
+            }
+            else if(s.size()>0&&s.top()<arr[i])
+            {
+                v.push_back(s.top());
+            }
+            else if(s.size()>0&&s.top()>=arr[i])
+            {
+                while(s.size()>0&&s.top()>=arr[i])
                 {
-                    res[i]=arr[j];
-                    break;
+                    s.pop();
+                }
+                if(s.empty())
+                {
+                    v.push_back(-1);
+                }
+                else 
+                {
+                    v.push_back(s.top());
                 }
             }
+            s.push(arr[i]);
         }
-        return res;
+        
+        reverse(v.begin(),v.end());
+        return v;
     } 
 };
 
