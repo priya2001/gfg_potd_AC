be3b0b579e00938bb143764a50ed757e62c02752
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-08-27 16:53:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Difference/maximum-difference.cpp
+++ Difficulty: Medium/Maximum Difference/maximum-difference.cpp
@@ -12,41 +12,37 @@ class Solution {
         int n=arr.size();
         stack<int>st;
         vector<int>left(n,0),right(n,0);
-        
-       for(int i=0;i<n;i++)
-       {
-           while(!st.empty()&&st.top()>=arr[i])
-           {
-               st.pop();
-           }
-               if(!st.empty())
-               {
-                   left[i]=st.top();
-               }
-              st.push(arr[i]);
-           
-       }
-       while(!st.empty())st.pop();
-       for(int i=n-1;i>=0;i--)
-       {
-           while(!st.empty()&&st.top()>=arr[i])
-           {
-               st.pop();
-           }
-               if(!st.empty())
-               {
-                   right[i]=st.top();
-               }
-               
-               st.push(arr[i]);
-           
-       }
-       int maxi=INT_MIN;
-       for(int i=0;i<n;i++)
-       {
-           maxi=max(maxi,abs(left[i]-right[i]));
-       }
-       return maxi;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty()&&st.top()>=arr[i])
+            {
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                left[i]=st.top();
+            }
+            st.push(arr[i]);
+        }
+        while(!st.empty())st.pop();
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty()&&st.top()>=arr[i])
+            {
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                right[i]=st.top();
+            }
+            st.push(arr[i]);
+        }
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            maxi=max(maxi,abs(left[i]-right[i]));
+        }
+        return maxi;
     }
 };
 
